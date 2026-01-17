6a0a8d5883af5cda14989e58ca8560a27870197f
khedayatullah452-lgtm <khedayatullah452@gmail.com>
2026-01-17 13:16:29 +0500

    Task1

--- Tyuiu.kkhalid.Sprint5.Task1.V17.Lib/DataService.cs
+++ Tyuiu.kkhalid.Sprint5.Task1.V17.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-
+using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
-
+using Tyuiu.kkhalid.Sprint5.Task1.V17.Lib;
 
 namespace Tyuiu.kkhalid.Sprint5.Task1.V17.Lib
 {
@@ -9,41 +9,49 @@ namespace Tyuiu.kkhalid.Sprint5.Task1.V17.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
 
+            // Удаляем файл если существует
+            if (File.Exists(path))
             {
-                string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+                File.Delete(path);
+            }
 
-                using (StreamWriter writer = new StreamWriter(path))
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                writer.WriteLine("Табулирование функции F(x) = 2x - 4 + (2x - 1)/(sin(x) + 1)");
+                writer.WriteLine("Диапазон: [" + startValue + "; " + stopValue + "]");
+                writer.WriteLine("Шаг: 1");
+                writer.WriteLine();
+                writer.WriteLine("x\t\tF(x)");
+                writer.WriteLine("-------------------");
+
+                for (int x = startValue; x <= stopValue; x++)
                 {
-                    writer.WriteLine("Табулирование функции F(x) = 2x - 4 + (2x-1)/(sin(x)+1)");
-                    writer.WriteLine("на диапазоне [" + startValue + "; " + stopValue + "] с шагом 1");
-                    writer.WriteLine();
-                    writer.WriteLine("+----------+----------+");
-                    writer.WriteLine("|    x     |   f(x)   |");
-                    writer.WriteLine("+----------+----------+");
-
-                    for (int x = startValue; x <= stopValue; x++)
+                    try
                     {
+                        // Проверка деления на ноль
                         double denominator = Math.Sin(x) + 1;
 
-                        // Проверка деления на ноль
-                        if (Math.Abs(denominator) < 0.000001)
+                        if (Math.Abs(denominator) < 0.000001) // если sin(x) ≈ -1
                         {
-                            writer.WriteLine($"|{x,10:F2}|{0,10:F2}|");
+                            writer.WriteLine($"{x}\t\t0.00");
                         }
                         else
                         {
-                            double f = 2 * x - 4 + (2 * x - 1) / denominator;
-                            f = Math.Round(f, 2);
-                            writer.WriteLine($"|{x,10:F2}|{f,10:F2}|");
+                            double numerator = 2 * x - 1;
+                            double value = 2 * x - 4 + numerator / denominator;
+                            writer.WriteLine($"{x}\t\t{Math.Round(value, 2):F2}");
                         }
                     }
-
-                    writer.WriteLine("+----------+----------+");
+                    catch
+                    {
+                        writer.WriteLine($"{x}\t\t0.00");
+                    }
                 }
-
-                return path;
             }
+
+            return path;
         }
     }
 }
--- Tyuiu.kkhalid.Sprint5.Task1.V17/Program.cs
+++ Tyuiu.kkhalid.Sprint5.Task1.V17/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Tyuiu.kkhalid.Sprint5.Task1.V17.Lib;
 
 namespace Tyuiu.kkhalid.Sprint5.Task1.V17
@@ -7,50 +8,62 @@ namespace Tyuiu.kkhalid.Sprint5.Task1.V17
     {
         static void Main(string[] args)
         {
-            // Интерфейс по шаблону
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* F(x) = 2x - 4 + (2x - 1)/(sin(x) + 1)                                   *");
-            Console.WriteLine("* Табулирование на диапазоне [-5; 5] с шагом 1                           *");
-            Console.WriteLine("* При делении на ноль вернуть 0                                           *");
-            Console.WriteLine("* Округлить до двух знаков после запятой                                  *");
-            Console.WriteLine("* Результат сохранить в файл OutPutFileTask1.txt                          *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-            Console.WriteLine("***************************************************************************");
-
             int startValue = -5;
             int stopValue = 5;
 
             DataService ds = new DataService();
-            string path = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            // Чтение и вывод содержимого файла
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Успешно создан!");
+            Console.WriteLine();
 
             // Вывод таблицы на консоль
-            Console.WriteLine("+----------+----------+");
-            Console.WriteLine("|    x     |   f(x)   |");
-            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("Таблица значений функции:");
+            Console.WriteLine("x\t\tF(x)");
+            Console.WriteLine("-------------------");
 
             for (int x = startValue; x <= stopValue; x++)
             {
-                double denominator = Math.Sin(x) + 1;
+                try
+                {
+                    double denominator = Math.Sin(x) + 1;
 
-                if (Math.Abs(denominator) < 0.000001)
-                {
-                    Console.WriteLine($"|{x,10:F2}|{0,10:F2}|");
+                    if (Math.Abs(denominator) < 0.000001)
+                    {
+                        Console.WriteLine($"{x}\t\t0.00");
+                    }
+                    else
+                    {
+                        double numerator = 2 * x - 1;
+                        double value = 2 * x - 4 + numerator / denominator;
+                        Console.WriteLine($"{x}\t\t{Math.Round(value, 2):F2}");
+                    }
                 }
-                else
+                catch
                 {
-                    double f = 2 * x - 4 + (2 * x - 1) / denominator;
-                    f = Math.Round(f, 2);
-                    Console.WriteLine($"|{x,10:F2}|{f,10:F2}|");
+                    Console.WriteLine($"{x}\t\t0.00");
                 }
             }
 
-            Console.WriteLine("+----------+----------+");
-            Console.WriteLine($"Файл: {path}");
-            Console.WriteLine("Создан!");
-
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ВЫПОЛНЕНИЕ ЗАВЕРШЕНО                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Нажмите любую клавишу для завершения...");
             Console.ReadKey();
         }
     }
